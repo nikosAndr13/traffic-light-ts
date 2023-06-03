6623e16629643c2bd3cf7de6a67e028c764be4d4
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2023-06-03 21:18:34 +0300

    Finish Class and Functional Component

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,48 @@
 import { Component } from "react";
+import { colors } from "./FunctionalTrafficLight";
 
+interface State {
+  colors: colors[];
+  activeColor: colors;
+}
 export class ClassTrafficLight extends Component {
-  render() {
+  state: State = {
+    colors: ["red", "green", "yellow"] as colors[],
+    activeColor: "red" as colors,
+  };
+
+  changeColor = (): void => {
+    const { colors, activeColor } = this.state;
+    const currentIndex = colors.indexOf(activeColor);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    const nextColor = colors[nextIndex];
+    this.setState({ activeColor: nextColor });
+  };
+
+  render(): JSX.Element {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div
+            className={`circle ${
+              this.state.activeColor === "red" ? "red" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.activeColor === "yellow" ? "yellow" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.activeColor === "green" ? "green" : "black"
+            }`}
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.changeColor}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,35 @@
-export const FunctionalTrafficLight = () => {
+import { useState } from "react";
+
+export type colors = "red" | "green" | "yellow";
+
+export const FunctionalTrafficLight = (): JSX.Element => {
+  const colors: colors[] = ["red", "green", "yellow"];
+  const [activeColor, setActiveColor] = useState<colors>("red");
+
+  const changeColor = () => {
+    const currentIndex = colors.indexOf(activeColor);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    const nextColor = colors[nextIndex];
+    setActiveColor(nextColor);
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div
+          className={`circle ${activeColor === "red" ? "red" : "black"}`}
+        ></div>
+        <div
+          className={`circle ${activeColor === "yellow" ? "yellow" : "black"}`}
+        ></div>
+        <div
+          className={`circle ${activeColor === "green" ? "green" : "black"}`}
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={changeColor}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
